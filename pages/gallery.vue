2846a27490fc8c12ed76cb53097e0c6a4655572a
HB <156240575+hbuhrer@users.noreply.github.com>
2024-09-09 12:07:01 -0600

    Gallery is completed. No known bugs.

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -3,7 +3,7 @@
       <GalleryHero />
       <div class="border-b-2 max-w-[1150px] mx-auto min-h-2 pt-12 px-6"></div>
       <section class="flex flex-col mt-10 lg:mt-0 lg:flex-row justify-center max-w-7xl mx-auto">
-        
+        <GalleryGrid />
       </section>
     </div>
   </template>
